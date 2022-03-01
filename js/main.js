cb1b5fe59d35eba095cba75d8051ccb5b0132c04
Manuel Gerolin <manuel31gerolin@gmail.com>
2022-03-01 22:59:26 +0100

    some prob with cycle for / if - carousel

--- js/main.js
+++ js/main.js
@@ -22,18 +22,52 @@ const text = [
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
 
-// aggiungo le immagini al file html
-let carouselContent  = '';
+// mi trasformo i miei elementi in variabili const che poi non tocco più
 
-for (let i = 0 ; i < items.length ; i++){
-    carouselContent  += `
-    <div class="my-img">
-        <img src="${items[i]}" alt="Random picture">
-    </div>`
+const myImg = document.getElementById('my-img-main');
+const myTitle = document.getElementById('title');
+const description = document.getElementById('description');
+const carouselContainer = document.getElementById('carousel-container');
+
+//mi serve un indicatore che conti dove sono partendo da zero index [0]
+
+let elementIndex = 0;
+
+for (let i = 0; i < items.length; i++) {
+    if (i === 0) {
+        myImg.src = items[i];
+        myTitle.innerHTML = title[i];
+        description.innerHTML = text[i];
+    }
+    // aggiungo le immagini al file html
+    // let carouselContent  = '';
+    // aggiungo la variabile che va a crearmi img dentro il div l
+    const myNewImg = document.createElement('img');
+    //devo aggiungere le immagini del carosello lateraale
+    //adesso mynewimg assume il vaolore del iesimo item
+    myNewImg.src = items[i];
+    myNewImg.classList.add('carousel-side');
+    // condizione per non spaccare tutto che non va più avanti if
+    //mi serve la variabile container carousel
+    if (i === 0) myNewImg.classList.add('active');
+    carouselContainer.appendChild(myNewImg);
 }
 
+document.getElementById('up').addEventListener(`click`, function () {
+    const imgList = document.querySelector('.carousel-side');
 
-const carouselWrapper = document.querySelector('div.carousel-container');
+    for (let i = 0; i < imgList.length; i++) {
+        imgList[i].classList.remove('active');
+    }
+    //ripesco l'elementindex
+    elementIndex++; // elementindex = elementindex + 1
+    myImg.src = items[elementIndex];
+    imgList[elementIndex].classList.add('active');
+    myTitle.innerHTML = title[elementIndex];
+    description.innerHTML = text[elementIndex];
+} )
+
+/* const carouselWrapper = document.querySelector('div.carousel-container');
 carouselWrapper.innerHTML += carouselContent ;
 //ora ho le img non visibili nel file html all'interno di my-img
 
@@ -56,4 +90,4 @@ nextBtn.addEventListener('click', function () {
     carouselElements[activeElement].classList.remove('my-active');
     activeElement++;
     carouselElements[activeElement].classList.add('my-active');
-});
+}); */
